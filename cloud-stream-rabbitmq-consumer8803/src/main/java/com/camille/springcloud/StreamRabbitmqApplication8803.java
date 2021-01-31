2b111204d893fef6bbd12ca2f47b4b778d8faad4
pengkangzaia <pengkangzaia@foxmail.com>
2021-01-31 16:59:00 +0800

    stream克隆一个消费者module

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/camille/springcloud/StreamRabbitmqApplication8803.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright: 2021 dingxiang-inc.com Inc. All rights reserved.
+ */
+
+package com.camille.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @FileName: StreamRabbitmqApplication8802.java
+ * @Description: StreamRabbitmqApplication8802.java类说明
+ * @Author: kang.peng
+ * @Date: 2021/1/31 16:05
+ */
+@SpringBootApplication
+public class StreamRabbitmqApplication8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbitmqApplication8803.class, args);
+    }
+
+}
