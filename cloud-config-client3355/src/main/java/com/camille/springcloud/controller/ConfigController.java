22bd39d9b399505f90b8f223f673ab54544e2bde
pengkangzaia <pengkangzaia@foxmail.com>
2021-01-31 11:43:46 +0800

    config服务配置客户端动态刷新（手动刷新）

--- cloud-config-client3355/src/main/java/com/camille/springcloud/controller/ConfigController.java
+++ cloud-config-client3355/src/main/java/com/camille/springcloud/controller/ConfigController.java
@@ -5,6 +5,7 @@
 package com.camille.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2021/1/31 11:24
  */
 @RestController
+@RefreshScope  // 实现动态刷新功能，同时还需要向客户端发送一个POST请求，来刷新配置 “http://localhost:3355/actuator/refresh”
 public class ConfigController {
 
     @Value("${config.info}")
